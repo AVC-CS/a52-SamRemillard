7d72d339933f00e0da135598be0a517f80e707eb
Kyu Lee <kwooler@gmail.com>
2026-03-11 00:27:45 -0700

    Student starter code

--- main.cpp
+++ main.cpp
@@ -3,16 +3,6 @@
 int main()
 {
     // TODO
-    int num, maxVal;
-    std::cin >> maxVal;
-    for (int i = 1; i < 10; i++) {
-        std::cin >> num;
-        if (num > maxVal) {
-            maxVal = num;
-        }
-    }
-    std::cout << "Maximum value: " << maxVal << std::endl;
-    // END TODO
 
     return 0;
 }
